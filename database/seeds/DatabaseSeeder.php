e58c66974afb08da260c34da8ae01062896106f6
Daichi Ando <d_ando@gizumo-admin-07noMacBook-puro.local>
2018-07-17 18:53:09 +0900

    adminseed_reset

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(AdminsTableSeeder::class);
+        $this->call(AdminUsersTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(DailyReportsTableSeeder::class);
         $this->call(StoresTableSeeder::class);
