54d467403c19d39d0e560bea7b1917282f83b434
tsubasa111 <tabletennis283@gmail.com>
2018-07-20 15:19:21 +0900

    add_userTable_to_seederfile

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,13 +12,15 @@ class UsersTableSeeder extends Seeder
       'name' => 'test',
       'password' => bcrypt('1234'),
       'user_info_id' => 1
-    ],[
+    ]);
+    User::create([
       'name' => 'test1',
-      'password' => bcrypt('2345'),
+      'password' => bcrypt('1234'),
       'user_info_id' => 2
-    ],[
+    ]);
+    User::create([
       'name' => 'test2',
-      'password' => bcrypt('3456'),
+      'password' => bcrypt('1234'),
       'user_info_id' => 3
     ]);
   }
