39668abbd85d47a3a9c371282fb6247ba2ff821e
kazuya_ochi <ochi0612@gmail.com>
2018-08-07 11:09:57 +0900

    Add is_registered, birthday, hire_date to seeder file

--- database/seeds/UserInfosTableSeeder.php
+++ database/seeds/UserInfosTableSeeder.php
@@ -15,9 +15,13 @@ class UserInfosTableSeeder extends Seeder
         'email' => 'hoge@gmail.com',
         'tel' => 11111111,
         'sex' => '男',
+        'birthday' => '1997-06-12',
+        'hire_date' => '2017-12-3',
         'store_id' => 1,
         'access_right' => 6,
-        'position_code' => 75
+        'position_code' => 75,
+        'is_registered' => 1 
+
     ]);
     UserInfos::create([
         'first_name' => '大地',
@@ -25,9 +29,13 @@ class UserInfosTableSeeder extends Seeder
         'email' => 'fuga@gmail.com',
         'tel' => 222222222,
         'sex' => '男',
+        'birthday' => '1993-10-24',
+        'hire_date' => '2017-04-3',
         'store_id' => 2,
         'access_right' => 7,
-        'position_code' => 25
+        'position_code' => 25,
+        'is_registered' => 1 
+
     ]);
     UserInfos::create([
         'first_name' => '翔平',
@@ -35,9 +43,13 @@ class UserInfosTableSeeder extends Seeder
         'email' => 'hogefuga@gmail.com',
         'tel' => 333333333,
         'sex' => '男',
+        'birthday' => '1991-10-26',
+        'hire_date' => '2018-07-16',
         'store_id' => 3,
         'access_right' => 0,
-        'position_code' => 100
+        'position_code' => 100,
+        'is_registered' => 1 
+
     ]);
     }
   }
